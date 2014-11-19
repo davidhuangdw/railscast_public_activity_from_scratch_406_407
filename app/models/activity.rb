67239b57b6ae558198b957c5465bff3de04a40de
David <davidhuangdw@163.com>
2014-11-19 19:45:57 +0800

    readme

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,8 @@ class Activity < ActiveRecord::Base
   belongs_to :user
   belongs_to :trackable, polymorphic: true
 
-  belongs_to :comment, ->{includes(:activities).references(:activities)}, foreign_key: :trackable_id
-  belongs_to :recipe,->{includes(:activities).references(:activities)}, foreign_key: :trackable_id
+  # belongs_to :comment, ->{includes(:activities).references(:activities)}, foreign_key: :trackable_id
+
+  belongs_to :comment, foreign_key: :trackable_id
+  belongs_to :recipe, foreign_key: :trackable_id
 end
